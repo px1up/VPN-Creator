23cfb51918bbab4935cb40ddf7447c2766251b40
Vladimir Yakovlev <40215129+px1up@users.noreply.github.com>
2020-12-28 23:38:20 +0300

    Add files via upload

--- ovpn_create.py
+++ ovpn_create.py
@@ -1,43 +1,50 @@
 #!/usr/bin/python3
 import os
 import time
-
-#путь к RSA
-rsa_path = ('/etc/openvpn/rsa')
+import random
+import subprocess
 
 name = str(input('Введите имя пользователя: '))
-os.chdir(rsa_path)
-os.system('. ./vars')
-#задержка чтобы успели подключится vars
-time.sleep(4)
-os.system('./build-key-pass ' + name)
 
-#если сертификаты не подписываются автоматически
-#os.system('/easyrsa gen-req ' + name)
-#подписать сертификат
-#os.system('./easyrsa sign-req client ' + name)
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = int(1)
+length = int(8)
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+
+Fout = open ( "/home/OVPN/pass.txt","w" )
+Fout.write(password)
+os.system('mv /home/OVPN/pass.txt '+ '/home/OVPN/' + name +'-pass.txt')
+
+
+print('\n' ,'\033[1;31;40m При создании сертификата введите сгенерированный пароль для пользователя:\n ', '\033[1;33;40m', password, '\033[0;37;40m')
+
+
+subprocess.check_call(['./easyrsa', 'build-client-full', name], cwd='/usr/share/easy-rsa/3')
 
 #Копирование файлов сертификата в директорию
-os.system('cp /etc/openvpn/rsa/keys/' + name + '.crt ' + '/etc/openvpn/rsa/keys/' +  name +'.key '+ '/etc/openvpn/rsa/keys/ca.crt /etc/openvpn/rsa/keys/ta.key /home/tmp')
-os.system('chmod -R a+r /home/tmp/')
+os.system('cp /usr/share/easy-rsa/3/pki/issued/' + name + '.crt ' + '/usr/share/easy-rsa/3/pki/private/' +  name +'.key '+ '/etc/openvpn/keys/ca.crt /etc/openvpn/keys/ta.key /tmp/keys')
+os.system('chmod -R a+r /tmp/keys')
 
 #Необходимо создать папку /tmp/OVPN_temp
 #Чистим временную папку OVPN
 os.system('rm -r /tmp/OVPN_temp/*')
 #Копируем во временную папку
-os.system('cp /home/tmp/' + name + '.crt ' + '/home/tmp/' +  name +'.key '+ '/home/tmp/ca.crt /home/tmp/ta.key /home/tmp/template /tmp/OVPN_temp')
+os.system('cp /tmp/keys/' + name + '.crt ' + '/tmp/keys/' +  name +'.key '+ '/tmp/keys/ca.crt /tmp/keys/ta.key /tmp/keys/template /tmp/OVPN_temp')
 
 os.chdir('/tmp/OVPN_temp')
 os.rename('ta.key','ta')
 
 #Поучаем файл с расширением *.key
 for file in os.listdir():
-    if file.endswith(".key"):
+    if file.endswith(name + ".key"):
         key = (os.path.join(file))
 
 #Поучаем файл с расширением *.crt
 for file in os.listdir():
-    if file.endswith(".crt"):
+    if file.endswith(name + ".crt"):
         crt = (os.path.join(file))
 
 for file in os.listdir():
@@ -93,7 +100,7 @@ with open(key) as file:
 #Создаем файл *.ovpn на основе config.ovpn
 with open("client.ovpn", "w") as file:
     #file.write(config + "\n" + "key-direction 1\n")
-    file.write(config  + "key-direction 1\n")
+    file.write(config  + "\nkey-direction 1\n")
 
 #Записываем ca
 with open("client.ovpn", "a") as file:
@@ -114,9 +121,8 @@ with open("client.ovpn", "a") as file:
 os.rename('ta','ta.key')
 os.rename('client.ovpn', name + ".ovpn")
 
-
-
 os.system('cp /tmp/OVPN_temp/' + name + '.ovpn' + ' /home/OVPN/')
+os.system('chmod 777 -R /home/OVPN/')
 print("Файл успешно создан")
 
 exit()
